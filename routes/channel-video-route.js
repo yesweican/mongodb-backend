162015bd41425ec636291468d3aaf63078cf5466
Wei Cheng <goedmonton@gmail.com>
2026-03-04 13:21:14 -0700

    Removing aunthentication from ChannelVideo route

--- routes/channel-video-route.js
+++ routes/channel-video-route.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import { getChannelVideos } from '../api/channel-controller.js';
-import authenticateToken from "../middleware/auth_middleware.js";
 
 const router = express.Router();
 
 // Route to get all videos of a channel
-router.get("/:id", authenticateToken, getChannelVideos);
+router.get("/:id", getChannelVideos);
 
 export default router;
